494220a85fe91bfd9a16348f6e231a79448d2633
joshruff <joshruff@tamu.edu>
2015-11-06 14:11:43 -0600

    Added Workaround for Json

--- WaterSensorTest/WaterSensorTest.ino
+++ WaterSensorTest/WaterSensorTest.ino
@@ -4,7 +4,7 @@
 int ledPin = 6;
 volatile int NbTopsFan; //measuring the rising edges of the signal
 int Calc;                               
-int hallsensor = 4;
+int hallsensor = 26;
 boolean ledState=HIGH;//The pin location of the sensor
 
 StaticJsonBuffer<200> jsonBuffer;//reserve memory space for Json
@@ -16,9 +16,10 @@ void setup() {
   json["flow_rate"]=Calc;
   json["units"]="L/Hr";
   ledState=HIGH;
-  ledPin = 6;
+  ledPin = 6;  
   pinMode(ledPin, OUTPUT);
   pinMode(hallsensor, INPUT); //initializes hall sensor pin as an input
+  delay(1000);
   Serial1.begin(9600); //This is the setup function where the serial port is initialised,
   Serial.begin(9600);
   attachInterrupt(digitalPinToInterrupt(hallsensor), rpm, RISING); //and the interrupt is attached
@@ -35,20 +36,24 @@ void loop ()
   sei();      //Enables interrupts
     delay(1000);
   cli();      //Disable interrupts
-  Calc = (NbTopsFan * 60 / 73); //(Pulse frequency x 60) / 73Q, = flow rate in L/hour 
+  //Serial.println(NbTopsFan);
+  Calc = Calc+1;//(NbTopsFan * 60 / 73); //(Pulse frequency x 60) / 73Q, = flow rate in L/hour 
   json["flow_rate"]=Calc;
-  json.printTo(Serial);
-  Serial.println();
-  json.printTo(Serial1);
-  Serial1.println();
+  //json.printTo(Serial);
+  //Serial.println();
+  Serial1.println(Calc);
+  //json.printTo(Serial1);
+  //Serial1.printf("{\"Sensor\":\"flow\",\"flow_rate\":%d\"units\"",Calc);
+  ////\"Sensor\":\"flow\",\"flow_rate\":%d,\"units\":\"L/Hr\"}",Calc);
+  Serial.printf("{\"Sensor\":\"flow\",\"flow_rate\":%d,\"units\":\"L/Hr\"}\n",Calc);
 }
  
 
  
 void rpm ()     //This is the function that the interupt calls 
 { 
-  Serial.println(NbTopsFan++);  //This function measures the rising and falling edge of the hall effect sensors signal
- 
+  Serial.println("here");
+  NbTopsFan++;  //This function measures the rising and falling edge of the hall effect sensors signal 
 } 
 
 //this function blinks the led with a delay passed as parameter
@@ -59,3 +64,8 @@ void blink_led_pin(int del)
   digitalWrite(ledPin, LOW);
   delay(del); 
 }
+
+
+
+  
+
